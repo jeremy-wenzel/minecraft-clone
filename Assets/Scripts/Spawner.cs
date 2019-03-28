adb1af6490548c6c39e2fdcfa9c5dcf3699f1b52
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2019-03-27 20:28:16 -0400

    Created new way to batch chunk spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,7 +11,7 @@ public class Spawner : MonoBehaviour
     private Chunk currentChunk = null;
     private const int BUILD_WIDTH = 5;
     private HashSet<Chunk> visibleChunks = new HashSet<Chunk>();
-
+    private Queue<Vector3> chunkCreationSet = new Queue<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
@@ -39,6 +39,7 @@ public class Spawner : MonoBehaviour
     {
         if (!currentChunk.IsPositionInChunk(player.transform.position))
         {
+            chunkCreationSet.Clear();
             //Debug.Log("Resetting chunk");
             Chunk newChunk = ChunkManager.GetChunkWithKey(Chunk.GetKey(player.transform.position));
 
@@ -51,7 +52,7 @@ public class Spawner : MonoBehaviour
             float xDiff = newChunk.StartX - currentChunk.StartX;
             float zDiff = newChunk.StartZ - currentChunk.StartZ;
 
-            HashSet<Chunk> newChunks = new HashSet<Chunk>();
+            HashSet<string> newChunks = new HashSet<string>();
             for (float xOffset = -Chunk.CHUNK_SIZE * BUILD_WIDTH; xOffset < Chunk.CHUNK_SIZE * BUILD_WIDTH; xOffset += Chunk.CHUNK_SIZE)
             {
                 for (float zOffset = -Chunk.CHUNK_SIZE * BUILD_WIDTH; zOffset < Chunk.CHUNK_SIZE * BUILD_WIDTH; zOffset += Chunk.CHUNK_SIZE)
@@ -59,24 +60,20 @@ public class Spawner : MonoBehaviour
                     Vector3 pos = new Vector3(newChunk.StartX + xOffset, 0, newChunk.StartZ + zOffset);
                     if (!ChunkManager.ChunkExists(Chunk.GetKey(pos)))
                     {
-                        var newChunkZ = Instantiate(PrefabManager.GetPrefab(PrefabType.Chunk), pos, new Quaternion());
-                        var chunk = (Chunk)newChunkZ.GetComponent(typeof(Chunk));
-                        ChunkManager.AddChunk(chunk);
-
-                        newChunks.Add(chunk);
+                        chunkCreationSet.Enqueue(pos);
                     }
                     else
                     {
                         Chunk existingChunk = ChunkManager.GetChunkWithKey(Chunk.GetKey(pos));
                         existingChunk.SetVisibility(true);
-                        newChunks.Add(existingChunk);
+                        newChunks.Add(existingChunk.GetKey());
                     }
                 }
             }
             
             foreach (Chunk c in visibleChunks)
             { 
-                if (!newChunks.Contains(c))
+                if (!newChunks.Contains(c.GetKey()))
                 {
                     if (c.IsChanged)
                     {
@@ -89,8 +86,24 @@ public class Spawner : MonoBehaviour
                     }
                 }
             }
-            visibleChunks = newChunks;
+
+            visibleChunks.Clear();
+            foreach (string s in newChunks)
+            {
+                if (ChunkManager.ChunkExists(s))
+                {
+                    visibleChunks.Add(ChunkManager.GetChunkWithKey(s));
+                }
+            }
             currentChunk = newChunk;
+        }
+
+        if (chunkCreationSet.Count > 0)
+        {
+            var pos = chunkCreationSet.Dequeue();
+            var newChunkZ = Instantiate(PrefabManager.GetPrefab(PrefabType.Chunk), pos, new Quaternion());
+            var chunk = (Chunk)newChunkZ.GetComponent(typeof(Chunk));
+            ChunkManager.AddChunk(chunk);
         }
     }
 }
