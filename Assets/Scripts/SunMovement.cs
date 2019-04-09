2ca8796fad17a3159df667fb2d4a737c88ec45ed
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2019-04-09 19:05:51 -0400

    Added some sun movement. Super basic but works

---
+++ Assets/Scripts/SunMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunMovement : MonoBehaviour
+{
+    private float currentTime = 0;
+    private const float speed = .5f;
+    private float startY = 100;
+    private float startX = 100;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime * speed;
+        this.transform.position = new Vector3(Mathf.Cos(currentTime) * startX, Mathf.Sin(currentTime) * startY, 0);
+
+        this.transform.LookAt(Vector3.zero);
+    }
+}
