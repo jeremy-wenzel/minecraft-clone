4136c7400c8dc8dd49e7fbec02c9fa766045b6a9
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2019-05-18 14:02:13 -0400

    Making sure we don't mine water cubes

---
+++ Assets/Scripts/Cubes/WaterCube.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Cubes
+{
+    public class WaterCube : Cube
+    {
+        public override void MineCube()
+        {
+            // Do nothing. Don't want to mine water cubes just yet
+        }
+    }
+}
